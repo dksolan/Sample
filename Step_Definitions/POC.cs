f5774b85b3ff8604a112a163516d4799f12511c1
Kumar <dinesh.x.kumar@capgemini.com>
2019-03-08 16:23:49 +0530

    Creating a master copy of my local repo
    Signed-off-by: Kumar <dinesh.x.kumar@capgemini.com>

---
+++ Step_Definitions/POC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Specflow;
+
+namespace WTW.Step_Definitions
+{
+    class POC 
+    {
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
